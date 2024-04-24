bb97cdf540b05cc7942374893c2f25743b7fd92d
FindMalek <malekgarahellalbus@gmail.com>
2024-04-24 23:39:43 +0100

    style: adjust the icon

--- reboot - an independent product team_files/index-4b81e072ba41f2a9.js
+++ reboot - an independent product team_files/index-4b81e072ba41f2a9.js
@@ -281,7 +281,7 @@
             onMouseMove: x,
             onMouseLeave: f,
             className:
-              "group pointer-events-none relative flex aspect-[4/3] h-full shrink-0 overflow-hidden rounded-lg bg-[#FBFBFB] bg-cover after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:border after:border-[rgba(0,0,0,0.08)] md:pointer-events-auto ",
+              "group pointer-events-none relative flex ] h-full shrink-0 overflow-hidden rounded-lg bg-[#FBFBFB] bg-cover after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:border after:border-[rgba(0,0,0,0.08)] md:pointer-events-auto ",
             animate: c,
             style: { backgroundImage: "url(".concat(t.poster, ")") },
             children: (0, a.jsxs)(u, {
@@ -317,7 +317,7 @@
           let { children: t, item: i } = e;
           return (0, a.jsx)("a", {
             href: i.url,
-            className: "relative inset-0 h-full w-full",
+            className: "relative inset-0 h-0 w-0",
             target: "_blank",
             rel: "noreferrer",
             children: t,
@@ -326,7 +326,7 @@
         g = (e) => {
           let { children: t } = e;
           return (0, a.jsx)("div", {
-            className: "relative inset-0 h-full w-full",
+            className: "relative inset-0 h-0 w-0",
             children: t,
           });
         },
@@ -350,8 +350,7 @@
           }, []);
         return (0, a.jsx)(r.E.section, {
           style: { x: t },
-          className:
-            "hidden h-[calc(100vh-505.5px)] max-h-[452px] min-h-[310px] space-x-8 pb-8 md:flex",
+          className: "hidden h-0 space-x-8 pb-8 md:flex",
           children: C.map((e, t) =>
             (0, a.jsx)(
               w,
@@ -795,14 +794,13 @@
                         ],
                       }),
                     }),
-                    // add checkout our projects [GITHUB ICON] on github
                     (0, a.jsx)("a", {
                       href: "https://github.com/function03-labs",
                       target: "_blank",
                       rel: "noreferrer",
                       className: "block",
                       children: (0, a.jsxs)("p", {
-                        className: "group",
+                        className: "group flex items-center",
                         children: [
                           (0, a.jsxs)("span", {
                             className:
@@ -811,12 +809,26 @@
                           }),
                           (0, a.jsx)("span", {
                             style: {
-                              // bold
                               fontWeight: "bold",
                             },
                             className:
-                              "font-medium underline transition-opacity duration-700 group-hover:!opacity-100 group-hover/footer:opacity-30 md:no-underline",
-                            children: "github.",
+                              "flex items-center font-medium underline transition-opacity duration-700 group-hover:!opacity-100 group-hover/footer:opacity-30 md:no-underline px-2", // add padding to the parent span element
+                            children: [
+                              (0, a.jsx)("svg", {
+                                "aria-hidden": "true",
+                                height: "20",
+                                version: "1.1",
+                                viewBox: "0 0 16 16",
+                                width: "20",
+                                className: "m-1", // add margin to the svg element
+                                children: (0, a.jsx)("path", {
+                                  rule: "evenodd",
+                                  d: "M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z",
+                                }),
+                              }),
+                              " ",
+                              "Github.",
+                            ],
                           }),
                         ],
                       }),
